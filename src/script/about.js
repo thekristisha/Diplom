5cd104b89c386d85e4d8b1de5eb36b1c9791e8a2
thekristisha <67333873+thekristisha@users.noreply.github.com>
2020-07-26 01:44:37 +0300

    Верстка всех страниц без адаптивности (без swiper, таблица аналитики требует доработки, секция search требует доработки). Настроен webpack

---
+++ src/script/about.js
@@ -0,0 +1,2 @@
+console.log('this is about page');
+import '../style/about.css';
---
+++ src/script/main.js
@@ -0,0 +1,4 @@
+console.log('this is main page');
+import '../style/main.css';
+// import '../style/main.css';
+// import './about.html'
