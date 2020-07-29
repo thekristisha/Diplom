4209ea05a217c93b95c1d8e8fde1731f293e99f8
thekristisha <67333873+thekristisha@users.noreply.github.com>
2020-07-29 21:08:27 +0300

    адаптация страницы about для iphone 5

--- src/script/about.js
+++ src/script/about.js
@@ -2,7 +2,7 @@ console.log('this is about page');
 import '../style/about.css';
 var elem = document.querySelector('.git-history__swiper');
 var flkty = new Flickity( elem, {
-  cellAlign: 'left',
+  cellAlign: 'center',
   contain: true,
   wrapAround: true,
   draggable: true,
